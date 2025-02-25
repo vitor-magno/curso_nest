36489881ecae9c21bb447e257444d0f48df58f79
Vitor <vitor-magno>
2025-02-25 07:55:03 -0300

    create user and bcrypt password user

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { Prisma, User } from '@prisma/client';
+import { UserService } from './user.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+
+    constructor(private userService: UserService) { }
+
+    @Post('signup')
+    async signupUser(@Body() data: Prisma.UserCreateInput): Promise<User> {
+        return this.userService.createUser(data);
+    }
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,42 @@
 import { Injectable } from '@nestjs/common';
+import { Prisma, User } from '@prisma/client';
+import { PrismaService } from 'src/database/prisma.service';
+import * as bcrypt from 'bcrypt';
+
 
 @Injectable()
-export class UserService {}
+export class UserService {
+
+    constructor(private primaService: PrismaService) { }
+
+    //Cria um usuário
+    async createUser(data: Prisma.UserCreateInput) {
+        const hashPassword = await bcrypt.hash(data.password, 10);
+        data.password = hashPassword
+        return this.primaService.user.create({ data });
+    }
+
+    //Pega um usuário específico
+    async user(
+        userWhereUniqueInput: Prisma.UserWhereUniqueInput,
+    ): Promise<User | null> {
+        return this.primaService.user.findUnique({
+            where: userWhereUniqueInput,
+        });
+    }
+
+    // Atualiza um usuário
+    async updateUser(params: {
+        where: Prisma.UserWhereUniqueInput;
+        data: Prisma.UserUpdateInput;
+    }): Promise<User> {
+        const { where, data } = params;
+        return this.primaService.user.update({ where, data });
+    }
+
+    // Deleta um usuário
+    async deleteUser(where: Prisma.UserWhereUniqueInput) {
+        return this.primaService.user.delete({ where });
+    }
+
+}
